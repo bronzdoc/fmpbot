97c8ad0a71668d7898565c33cece2e241762a760
bronzdoc <lsagastume1990@gmail.com>
2016-04-18 22:09:04 -0600

    Post to webhook

--- index.js
+++ index.js
@@ -9,11 +9,22 @@ var app = express();
 app.use(bodyParser.json());
 
 app.get("/webhook", (req, res) => {
-    console.log("got here");
     if (req.query['hub.verify_token'] === verifyToken)
         return res.send(req.query['hub.challenge']);
 
     res.send('Error, wrong validation token');
+});
+
+app.post("/webhook", (req, res) => {
+    var messaging_events = req.body.entry[0].messaging;
+    messaging_events.forEach((event) => {
+        var sender = event.sender.id;
+
+        if (event.message && event.message.text) {
+            text = event.message.text;
+        }
+    })
+    res.sendStatus(200);
 });
 
 app.listen(3000);
