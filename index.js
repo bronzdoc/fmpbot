71c74abdc0cf81e93b05c388ec60974f570280f9
bronzdoc <lsagastume1990@gmail.com>
2016-04-19 11:27:41 -0600

    Setup generic message

--- index.js
+++ index.js
@@ -24,7 +24,11 @@ app.post("/webhook", (req, res) => {
 
         if (event.message && event.message.text) {
             text = event.message.text;
-            sendTextMessage(sender, "Text received, echo: " + text);
+            if (text === "Generic") {
+                sendGenericMessage(sender);
+            } else {
+                sendTextMessage(sender, "Text received, echo: " + text);
+            }
         }
     })
     res.sendStatus(200);
@@ -53,6 +57,55 @@ function sendMessage (sender, message) {
             console.log("Error: ", res.body.error);
         }
     });
+}
+
+function sendGenericMessage(sender) {
+  messageData = {
+    "attachment": {
+      "type": "template",
+      "payload": {
+        "template_type": "generic",
+        "elements": [{
+          "title": "First card",
+          "subtitle": "Element #1 of an hscroll",
+          "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+          "buttons": [{
+            "type": "web_url",
+            "url": "https://www.messenger.com/",
+            "title": "Web url"
+          }, {
+            "type": "postback",
+            "title": "Postback",
+            "payload": "Payload for first element in a generic bubble",
+          }],
+        },{
+          "title": "Second card",
+          "subtitle": "Element #2 of an hscroll",
+          "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+          "buttons": [{
+            "type": "postback",
+            "title": "Postback",
+            "payload": "Payload for second element in a generic bubble",
+          }],
+        }]
+      }
+    }
+  };
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
 }
 
 app.listen(app.get("port"), () => {
