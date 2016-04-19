c08f3dc6d360f3cbaea7b5a363e4ef2e7535a5a9
bronzdoc <lsagastume1990@gmail.com>
2016-04-18 19:36:17 -0600

    Adding fb example webhook

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var request = require('superagent');
+var bodyParser = require('body-parser');
+
+var pageToken = process.env.APP_PAGE_TOKEN;
+var verifyToken = process.env.APP_VERIFY_TOKEN;
+
+var app = express();
+app.use(bodyParser.json());
+
+app.get("/webhook", (req, res) => {
+    if (req.query['hub.verify_token'] === verifyToken)
+        return res.send(req.query['hub.challenge']);
+
+    res.send('Error, wrong validation token');
+});
